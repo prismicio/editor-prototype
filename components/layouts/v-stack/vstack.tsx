9ddf6809d73608b79732e6db8fb9bfb995dbf538
BMA <benjammartin@gmail.com>
2022-10-03 23:19:59 +0200

    ok

--- components/layouts/v-stack/vstack.tsx
+++ components/layouts/v-stack/vstack.tsx
@@ -1,10 +1,14 @@
-import React, { Fragment } from "react";
+import React from "react";
+import clsx from "clsx";
 import { Box } from "components/layouts/box";
+import styles from "./vstack.module.css";
+import tokens from "token.config.json";
 
 interface VStackProps<T> {
   children: (item: T) => React.ReactNode;
   items: T[];
   as?: "ol" | "ul";
+  space?: keyof typeof tokens.space;
   className?: string;
   separator?: boolean;
 }
@@ -16,13 +20,20 @@ interface IdObj {
 export const VStack = <T extends IdObj>({
   items,
   children,
+  space = "xs",
   as = "ul",
   className,
   separator = false,
   ...restProps
 }: VStackProps<T>) => {
+  const VSpace = `var(--space-${space})`;
   return (
-    <Box className={className} as={as} {...restProps}>
+    <Box
+      style={{ gap: VSpace }}
+      className={clsx(styles.root, className)}
+      as={as}
+      {...restProps}
+    >
       {items.map((item) => {
         return (
           <Box key={item.id} as="li">
