9528c0c7b83abb7c4859e483d8de39dfbd825997
BMA <benjammartin@gmail.com>
2022-10-04 10:59:30 +0200

    init

---
+++ components/cards/slice/slice.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styles from './slice.module.css'
+
+
+export default function Slice() {
+    return <div className={styles.root}>Slice</div>
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,38 +2,15 @@ import { Box } from "components/layouts/box";
 import { VStack } from "components/layouts/v-stack/vstack";
 import type { NextPage } from "next";
 import styles from "styles/home.module.css";
-import Page from "public/icons/Page.svg";
-import Slicemachine from "public/icons/Slicemachine.svg";
-import { IconButton } from "components/controls/icon-button/icon-button";
+import Slice from "components/cards/slice/slice";
 
-const icons = {
-  Slicemachine: <Slicemachine />,
-  Page: <Page />
-};
 
-const items = [
-  { id: "2", name: "sss", icon: Slicemachine },
-  { id: "1", name: "sss", icon: Page },
-  { id: "3", name: "sss", icon: Page }
-];
+
 
 const Home: NextPage = () => {
   return (
-    <Box as="div" className={styles.navigation}>
-      <VStack
-        space="xs"
-        className={styles.stack}
-        as="ul"
-        items={items}
-        children={(item) => {
-          const Icon = item.icon;
-          return (
-            <IconButton>
-              <Icon />
-            </IconButton>
-          );
-        }}
-      />
+    <Box as="div" className={styles.slices}>
+     <Slice/>
     </Box>
   );
 };
