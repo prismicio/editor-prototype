ad6509d3945d8f320fbb12a27dfb82b060dab44f
BMA <benjammartin@gmail.com>
2022-10-24 10:49:09 +0200

    ok

--- components/controls/document-options/document-options.tsx
+++ components/controls/document-options/document-options.tsx
@@ -1,11 +1,8 @@
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import styles from './document-options.module.css'
 import More from '@mui/icons-material/More'
-import { useDispatch } from 'react-redux'
-import { Dispatch } from 'pages/_app'
 
 export function DocumentOptions() {
-  const dispatch = useDispatch<Dispatch>()
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger className={styles.trigger}>
