b7d394c4e8b56cacc91a7981194a805fb7fdff22
BMA <benjammartin@gmail.com>
2022-11-01 17:55:35 +0100

    ok

--- components/dialogs/root-dialog.tsx
+++ components/dialogs/root-dialog.tsx
@@ -12,5 +12,5 @@ export function RootDialog() {
   const dialog = useSelector((state: RootState) => state.dialog)
   if (!dialog.type) return null
   const Component = DIALOGS[dialog.type as keyof typeof DIALOGS]
-  return <Component {...dialog.props} />
+  return <Component {...(dialog.props as any)} />
 }
